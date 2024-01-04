1d3064ca70e8c6005a8f104327d057cd6cb321fc
ted zhang <ted.zhang666@gmail.com>
2024-01-04 14:16:44 -0500

    473. planning data submission - done

--- code/12-adv-starting-project/frontend/src/App.js
+++ code/12-adv-starting-project/frontend/src/App.js
@@ -20,8 +20,65 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import HomePage from "./pages/Home";
+import EventDetailPage, { loader as eventsDetailLoader } from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+import RootLayout from "./pages/Root";
+import EventsRootLayout from "./pages/EventsRoot";
+import ErrorPage from "./pages/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "events",
+        element: <EventsRootLayout />,
+        children: [
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
+          {
+            path: ":eventId",
+            id: 'event-detail',
+            loader: eventsDetailLoader,
+            children: [
+              {
+                index: true,
+                element: <EventDetailPage />,
+                
+              },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+              },
+            ]
+          },
+          
+          {
+            path: "new",
+            element: <NewEventPage />,
+          },
+          
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ code/12-adv-starting-project/frontend/src/pages/Root.js
@@ -0,0 +1,20 @@
+import { Outlet, /* useNavigation */ } from "react-router-dom";
+import MainNavigation from "../components/MainNavigation";
+
+
+function RootLayout() {
+    // const navigation = useNavigation();
+
+
+    return (
+        <>
+            <MainNavigation />
+            <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
+                <Outlet />
+            </main>
+        </>
+    );
+}
+
+export default RootLayout;
